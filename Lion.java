ee64d9776f2d5de442d0f693a1ae66a8e83c6d50
Alecxnader Harmeling <aharmeling0201@myduneland.org>
2020-09-15 08:34:13 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Lion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lion
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class Lion
+     */
+    public Lion()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void roar()
+    {
+        // put your code here
+        System.out.print("Roar! Roar!");
+    }
+}
