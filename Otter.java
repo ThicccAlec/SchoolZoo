55d55064ea6e54e970d3dee487287693cdf017e4
ThicccAlec <55599994+ThicccAlec@users.noreply.github.com>
2020-09-15 09:04:31 -0500

    Add files via upload

---
+++ Otter.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Lion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Otter
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class Lion
+     */
+    public Otter()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void arf()
+    {
+        // put your code here
+        System.out.print("Arf! Arf!");
+    }
+}
